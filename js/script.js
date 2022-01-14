571410aa03749a4d92687fedb5f2d79757338d6e
arish dangol <arish.dangol@.com>
2022-01-14 12:50:16 +0545

    fixed filter bug

--- js/script.js
+++ js/script.js
@@ -12,28 +12,28 @@ function bgAnimationItems(){
 bgAnimationItems();
 
 // filter portfolio items 
-const filterBtnsContainer = document.querySelector(".portfolio-filter");
-filterBtnsContainer.addEventListener("click", (e) => {
-    if(e.target.classList.contains("portfolio-filter-btn") &&
-    !e.target.classList.contains("active")){
-        filterBtnsContainer.querySelector(".active").classList.remove("active");
-        e.target.classList.add("active");
-        filterItems(e.target);
-    }
-});
+ const filterBtnsContainer = document.querySelector(".portfolio-filter");
+ filterBtnsContainer.addEventListener("click", (e) => {
+         if (e.target.classList.contains("portfolio-filter-btn") &&
+             !e.target.classList.contains("active")) {
+            filterBtnsContainer.querySelector(".active").classList.remove("active");
+             e.target.classList.add("active");
+            filterItems(e.target);
+         }
+     });
 
 function filterItems(filterBtn){
     const selectedCategory = filterBtn.getAttribute("data-filter");
     document.querySelectorAll(".portfolio-item").forEach((item) =>{
         const category = item.getAttribute("data-category").split(",");
         if(category.indexOf(selectedCategory) !== -1 || selectedCategory === "all"){
-            item.classList.add("show");
-        }
-        else{
-            item.classList.remove("show");
-            filterItems(e.target);
-        }
-    });
-}
+             item.classList.add("show");
+      }
+         else{
+             item.classList.remove("show");
+             
+         }
+     });
+ }
 // filter active category portfolio items
 filterItems(document.querySelector(".portfolio-filter-btn.active"));
